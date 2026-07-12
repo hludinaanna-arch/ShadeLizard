12f091f4a51b135f83009d7ef7ce4a5beaea1aa0
hludinaanna-arch <hludinaanna@gmail.com>
2026-07-12 11:21:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -84,6 +84,7 @@ def handle_message(data):
     rooms_messages[room].append(data)
     emit('message', data, room=room)
 
-import os
-port = int(os.environ.get('PORT', 5000))
-socketio.run(app, host='0.0.0.0', port=port, debug=False, allow_unsafe_werkzeug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
